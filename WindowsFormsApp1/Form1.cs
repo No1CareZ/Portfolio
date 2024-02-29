084b8e4af61a17f7b138eb9ae2304ff732616ab5
theSaladin <99478272+No1CareZ@users.noreply.github.com>
2024-02-29 23:26:10 +0700

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length != textBox3.Text.Length && label4.Enabled)
+            {
+                label4.Visible = true;
+                button1.Enabled = false;
+            }
+            else
+            {
+                label4.Visible = false;
+                button1.Enabled = true;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length != textBox3.Text.Length && label4.Enabled)
+            {
+                label4.Visible = true;
+                button1.Enabled = false;
+            }
+            else
+            {
+                label4.Visible = false;
+                button1.Enabled = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length != 0 && !label4.Enabled)
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String mess = textBox1.Text;
+            String k = textBox2.Text;
+            String vi = textBox3.Text;
+            String txtsr;
+            Stopwatch sw = new Stopwatch();
+            sw.Restart();
+            sw.Start();
+
+            byte[] txt = Program.NormalizeMessage(mess, k);
+            byte[] key = Encoding.ASCII.GetBytes(k);
+            byte[] iv = Encoding.ASCII.GetBytes(vi);
+            byte[] enc = Program.EncryptionFunc(txt, key, iv);
+            txtsr = Program.NormalizeOutPut(enc);
+
+            textBox1.Text = txtsr;
+            label1.Visible = false;
+            label5.Visible = true;
+
+            sw.Stop();
+            TimeSpan time = sw.Elapsed;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox4.Text.Length != 0 && textBox5.Text.Length != 0)
+            {
+                button2.Enabled = true;
+                label10.Visible = false;
+            }
+            else
+            {
+                button2.Enabled = false;
+                label10.Visible = true;
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox4.Text.Length != 0 && textBox5.Text.Length != 0)
+            {
+                button2.Enabled = true;
+                label10.Visible = false;
+            }
+            else
+            {
+                button2.Enabled = false;
+                label10.Visible = true;
+            }
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox9.Text.Length !=0 && textBox8.Text.Length != 0)
+            {
+                button3.Enabled = true;
+                label12.Visible = false;
+            }
+            else
+            {
+                button3.Enabled = false;
+                label12.Visible = true;
+            }
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox9.Text.Length != 0 && textBox8.Text.Length != 0)
+            {
+                button3.Enabled = true;
+                label12.Visible = false;
+            }
+            else
+            {
+                button3.Enabled = false;
+                label12.Visible = true;
+            }
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox10.Text.Length == 0)
+            {
+                button3.Enabled = false;
+            }
+            else 
+            {
+                button3.Enabled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            byte[] iv = Encoding.ASCII.GetBytes(textBox8.Text);
+            byte[] message = Program.ReadEncry(textBox10.Text);
+            byte[] kt = Encoding.ASCII.GetBytes(textBox9.Text);
+            byte[] fif = Program.DecryptionFunc(message, kt, iv);
+            textBox10.Text = System.Text.Encoding.ASCII.GetString(fif, 0, fif.Length);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox7.Text = "Keys are:";
+            Task<string>[] threads = new Task<string>[6];
+            Stopwatch sw = new Stopwatch();
+
+            byte[] sig = Encoding.ASCII.GetBytes(textBox6.Text);
+            byte[] iv = Encoding.ASCII.GetBytes(textBox5.Text);
+            byte[] message = Program.ReadEncry(textBox4.Text);
+            byte[] kt = new byte[iv.Length];
+
+            sw.Restart();
+            sw.Start();
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                ulong bg = (ulong)i * (ulong)Math.Pow(255, iv.Length) / (ulong)threads.Length + 1;
+                ulong end = (ulong)(Math.Pow(255, iv.Length))/(ulong)(threads.Length) * (ulong)(i + 1);
+                threads[i] = Task.Factory.StartNew( () => Program.SerchForKey(bg, end, sig, message, iv));
+            }
+            Task.WaitAll(Task.WhenAll(threads));
+            for (int i = 0; i < threads.Length; i++)
+            {
+                textBox7.Text += threads[i].Result;
+            } 
+            sw.Stop();
+            TimeSpan time = sw.Elapsed;
+            double xc = time.TotalSeconds;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        static public byte[] EncryptionFunc(byte[] message, byte[] key, byte[] iv) 
+        {
+            byte[] block = new byte[key.Length];
+            byte[] result = new byte[message.Length];
+            byte[] vec = new byte[iv.Length];
+            Buffer.BlockCopy(iv, 0, vec, 0, vec.Length);
+
+            for (int i = 0; i < result.Length / key.Length; i++) 
+            { 
+                Buffer.BlockCopy(message, i * key.Length, block, 0, key.Length);
+                Buffer.BlockCopy(EncryptionFuncRecurent(block, key, vec), 0, vec, 0, vec.Length);
+                Buffer.BlockCopy(vec, 0, result, i * key.Length, key.Length);
+            }
+
+            return result;
+        }
+
+        static public byte[] DecryptionFunc(byte[] cipher, byte[] key, byte[] iv)
+        {
+            byte[] block = new byte[key.Length];
+            byte[] result = new byte[cipher.Length];
+            byte[] vec = new byte[iv.Length];
+            Buffer.BlockCopy(iv, 0, vec, 0, vec.Length);
+
+            for (int i = 0; i < cipher.Length / key.Length; i++)
+            {
+                Buffer.BlockCopy(cipher, i * key.Length, block, 0, key.Length);
+                Buffer.BlockCopy(EncryptionFuncRecurent(block, vec, key), 0, result, i * key.Length, key.Length);
+                Buffer.BlockCopy(cipher, i * key.Length, vec, 0, vec.Length);
+            }
+            return result;
+        }
+
+        static private byte[] EncryptionFuncRecurent(byte[] block, byte[] key, byte[] p_iv)
+        {
+            for (int j = 0; j < key.Length; j++)
+            {
+                Buffer.SetByte(block, j, (byte)(block[j] ^ p_iv[j]));
+            }
+            if (key != p_iv) return EncryptionFuncRecurent(block, key, key);
+            else return block;
+        }
+
+        static public byte[] NormalizeMessage(String mess, String k)
+        {
+            byte[] reader = new byte[mess.Length];
+            byte[] message;
+            int c = 0;
+            int a; int b;
+            if (mess.Length >= 4)
+            {
+                for (int i = 0; i < mess.Length - 4; i++)
+                {
+                    if (mess[i] == '0' && mess[i + 1] == 'x') // numb [48,57] and symb [65, 70]
+                    {
+                        if (((48 <= (int)mess[i + 2] && 57 >= (int)mess[i + 2]) || (65 <= (int)mess[i + 2] && 70 >= (int)mess[i + 2])))
+                        {
+                            if (((48 <= (int)mess[i + 3] && 57 >= (int)mess[i + 3]) || (65 <= (int)mess[i + 3] && 70 >= (int)mess[i])))
+                            {
+                                a = ((48 <= (int)mess[i + 2] && 57 >= (int)mess[i + 2])) ? (int)mess[i + 2] - 48 : (int)mess[i + 2] - 55;
+                                b = ((48 <= (int)mess[i + 3] && 57 >= (int)mess[i + 3])) ? (int)mess[i + 3] - 48 : (int)mess[i + 3] - 55;
+                                reader[c] = (byte)(a * 16 + b);
+                                i += 3;
+                            }
+                        }
+                    }
+                    else {
+                        reader[c] = (byte)mess[i];
+                    }
+                    c++;
+                }
+                reader[c] = (byte)mess[mess.Length - 4];
+                c++;
+                reader[c] = (byte)mess[mess.Length - 3];
+                c++;
+                reader[c] = (byte)mess[mess.Length - 2];
+                c++;
+                reader[c] = (byte)mess[mess.Length - 1];
+
+
+                message = new byte[c + 1];
+                Buffer.BlockCopy(reader, 0, message, 0, message.Length);
+            }
+            else message = Encoding.ASCII.GetBytes(mess);
+            byte[] key = Encoding.ASCII.GetBytes(k);
+
+            int scrap;
+            scrap = key.Length - message.Length % key.Length;
+             
+            byte[] result = new byte[scrap + message.Length];
+            Buffer.BlockCopy(message, 0, result, 0, message.Length);
+
+            for (int i = 0; i < scrap; i++)
+            {
+                Buffer.BlockCopy("!".ToArray(), 0, result, message.Length + i, 1); // adding !
+            }
+            Buffer.BlockCopy(BitConverter.GetBytes(scrap), 0, result, result.Length - 1, 1); // adding scrapbyte
+
+            return result;
+        }
+
+        static public string NormalizeOutPut(byte[] orig)
+        {
+            string s = string.Empty;
+            char a, b;
+            for (int i = 0; i < orig.Length; i++) {
+                if ((int)orig[i] <= 31)
+                {
+                    a = (int)orig[i] / 16 > 9 ? (char)(55 + (int)orig[i] / 16) : (char)(48 + (int)orig[i] / 16);
+                    b = (int)orig[i] % 16 > 9 ? (char)(55 + (int)orig[i] % 16) : (char)(48 + (int)orig[i] % 16);
+                    s += "0x" + (char)a + (char)b;
+                }
+                else s += (char)((int)orig[i]);
+            }
+
+            return s;
+        }
+
+        static public byte[] ReadEncry(string mess)
+        {
+            byte[] reader = new byte[mess.Length];
+            byte[] message;
+            int c = 0;
+            int a; int b;
+            if (mess.Length >= 4)
+            {
+                for (int i = 0; i < mess.Length - 4; i++)
+                {
+                    if (mess[i] == '0' && mess[i + 1] == 'x') // numb [48,57] and symb [65, 70]
+                    {
+                        if (((48 <= (int)mess[i + 2] && 57 >= (int)mess[i + 2]) || (65 <= (int)mess[i + 2] && 70 >= (int)mess[i + 2])))
+                        {
+                            if (((48 <= (int)mess[i + 3] && 57 >= (int)mess[i + 3]) || (65 <= (int)mess[i + 3] && 70 >= (int)mess[i])))
+                            {
+                                a = ((48 <= (int)mess[i + 2] && 57 >= (int)mess[i + 2])) ? (int)mess[i + 2] - 48 : (int)mess[i + 2] - 55;
+                                b = ((48 <= (int)mess[i + 3] && 57 >= (int)mess[i + 3])) ? (int)mess[i + 3] - 48 : (int)mess[i + 3] - 55;
+                                reader[c] = (byte)(a * 16 + b);
+                                i += 3;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        reader[c] = (byte)mess[i];
+                    }
+                    c++;
+                }
+                reader[c] = (byte)mess[mess.Length - 4];
+                c++;
+                reader[c] = (byte)mess[mess.Length - 3];
+                c++;
+                reader[c] = (byte)mess[mess.Length - 2];
+                c++;
+                reader[c] = (byte)mess[mess.Length - 1];
+
+
+                message = new byte[c + 1];
+                Buffer.BlockCopy(reader, 0, message, 0, message.Length);
+            }
+            else message = Encoding.ASCII.GetBytes(mess);
+            return message;
+        }
+
+        static public bool MooreSearch(byte[] sign, byte[] dec)
+        {
+            byte[] mor;
+            int end;
+            bool fl;
+
+
+            int lf = 0;
+            int count = 0;
+            int[] indexe = new int[sign.Length / 2];
+            indexe[0] = -2;
+            for (byte i = 0; i < sign.Length; i++)
+            {
+                if (sign[i] == Encoding.Unicode.GetBytes("%")[0])
+                {
+                    count++;
+                    indexe[count] = i - 1;
+                }
+            }
+
+            for (int j = 1; j < count + 1; j++)
+            {
+                byte[] sign_part = new byte[indexe[j] + 1 - (indexe[j-1] + 2)];
+                Buffer.BlockCopy(sign, indexe[j - 1] + 2, sign_part, 0, sign_part.Length);
+                mor = MakeMooreTable(sign_part);
+                end = sign_part.Length - 1;
+                while (end <= dec.Length - 1)
+                {
+                    fl = true;
+                    for (int i = 0; i < sign_part.Length; i++)
+                    {
+                        int mk = sign_part.Length - i - 1;
+                        if (sign_part[sign_part.Length - i - 1] != dec[end - i])
+                        {
+                            end += GetByteInMoorTable(mor, dec[end - i]) != 0 ? GetByteInMoorTable(mor, dec[end - i]) : sign_part.Length;
+                            fl = false;
+                            break;
+                        }
+                    }
+                    if (fl)
+                    {
+                        lf++;
+                        break;
+                    }
+                }
+            }
+            return lf == count;
+        }
+
+        static private byte[] MakeMooreTable(byte[] sign) {
+            byte[] result = new byte[sign.Length*2];
+            bool fl = true;
+            int j;
+            for (int i = 0; i < sign.Length; i++)
+            {
+                j = 0;
+                while (result[j] != 0)
+                {
+                    if (sign[i] == result[j])
+                    {
+                        fl = false;
+                        break;
+                    }
+                    j+=2;
+                }
+                if (fl)
+                {
+                    result[j] = sign[i];
+                    result[j + 1] = BitConverter.GetBytes(Math.Max(1, (sign.Length - i - 1)))[0];
+                }
+                else
+                {
+                    result[j] = sign[i];
+                    result[j + 1] = BitConverter.GetBytes(Math.Max(1, (sign.Length - i - 1)))[0];
+                }
+            }
+            return result;
+        }
+
+        static private byte GetByteInMoorTable(byte[] sign, byte ser)
+        {
+            for (int i = 0; i < sign.Length; i+=2) 
+            { 
+                if (ser == sign[i]) 
+                {
+                    return sign[i+1];
+                }
+            }
+            return 0;
+        }
+
+        static public string SerchForKey(ulong bg, ulong end, byte[] sign, byte[] mess, byte[] iv)
+        {
+            String result = "";
+            var name = Thread.CurrentThread.Name;
+            byte[] tk = new byte[iv.Length];
+            ulong c;
+            bool fl;
+            for (ulong i = bg; i < end; i++)
+            {
+                fl = true;
+                c = i;
+                for (int j = 1; j <= iv.Length; j++)
+                {
+                    tk[iv.Length - j] = (byte)(c % 256);
+                    c /= 256;
+                }
+
+                for (int j = 0; j < iv.Length - 2; j++)
+                {
+                    for (int k = j + 1; k < iv.Length - 1; k++)
+                    {
+                        if (tk[j] == tk[k])
+                        {
+                            fl = false;
+                            break;
+                        }
+                    }
+                    if (!fl) break;
+                }
+                if (fl == true)
+                {
+                    if (Program.MooreSearch(sign, Program.DecryptionFunc(mess, tk, iv)))
+                    {
+                        result += Environment.NewLine + System.Text.Encoding.ASCII.GetString(tk, 0, tk.Length);
+                    }
+                }
+                
+            }
+            return result;
+        }
+    }
+}
