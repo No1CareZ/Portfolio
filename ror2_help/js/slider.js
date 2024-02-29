66652b46f3c15b8c5ec21bc8608f933fefadb134
theSaladin <99478272+No1CareZ@users.noreply.github.com>
2024-02-29 23:29:49 +0700

    Delete ror2_help directory

--- ror2_help/js/slider.js
+++
@@ -1,8 +0,0 @@
-let lvle = document.getElementById('lvl');
-let markerv = document.getElementById('marker');
-
-markerv.innerHTML = "Selected lvl:" + lvle.value;
-
-lvle.oninput = function() {
-  markerv.innerHTML = "Selected lvl:" +  this.value;
-}
